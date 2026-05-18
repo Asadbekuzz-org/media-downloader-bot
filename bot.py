72e7422606868bb0c61d6c74c89f38c20d137162
Asadbekuzz-org <asadrax77@gmail.com>
2026-05-18 20:24:14 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,7 +11,7 @@ logging.basicConfig(level=logging.INFO)
 # 🔔 Ўз Телеграм ID рақамингизни шу ерга ёзинг!
 ADMIN_ID = 7705020569  
 
-TOKEN = os.getenv("8872513669:AAH8sY6wuLOYDS-eQpn6kCi3uZpDUjMTD8k")
+TOKEN = ("8872513669:AAH8sY6wuLOYDS-eQpn6kCi3uZpDUjMTD8k")
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
 
