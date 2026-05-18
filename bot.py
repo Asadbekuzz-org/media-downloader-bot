7fdede9763f9f23312c15f314ba32b2bf4a0a2db
Asadbekuzz-org <asadrax77@gmail.com>
2026-05-18 22:13:13 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -13,7 +13,7 @@ logging.basicConfig(level=logging.INFO)
 
 # --- БОТ СОЗЛАМАЛАРИ ---
 ADMIN_ID = 7705020569  
-TOKEN = "8872513669:AAH8sY6wuL0YDS-eQpn6kCi3uZpDUjMTD8k"
+TOKEN = "8872513669:AAH8sY6wuLOYDS-eQpn6kCi3uZpDUjMTD8k"
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
